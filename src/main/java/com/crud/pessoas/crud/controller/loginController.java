c7a67f9bcf25ad7c1329df7b309b79d040aca4d3
unknown <alisson_spada@hotmail.com>
2021-06-24 00:33:09 -0300

    controller

---
+++ src/main/java/com/crud/pessoas/crud/controller/loginController.java
@@ -0,0 +1,16 @@
+package com.crud.pessoas.crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class loginController {
+
+	@RequestMapping("/login")
+	public String login() {
+		return "/login/login";
+		
+	}
+	
+	
+}
