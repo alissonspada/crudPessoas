d14757a916b36bbd1f0314cb764a7b14ea78cdd5
Fabio Spada <fabioyudimaeda@hotmail.com>
2021-06-24 01:15:55 -0300

    feat: limpeza de classe crudPessoaApplication

--- src/main/java/com/crud/pessoas/crud/CrudPessoasApplication.java
+++ src/main/java/com/crud/pessoas/crud/CrudPessoasApplication.java
@@ -2,16 +2,9 @@ package com.crud.pessoas.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.crud.pessoas.crud")
-public class CrudPessoasApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(CrudPessoasApplication.class);
-	}
+@SpringBootApplication
+public class CrudPessoasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudPessoasApplication.class, args);
