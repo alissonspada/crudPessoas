33a05b14c2c59d41ffc82d3ae9db53151408a480
unknown <alisson_spada@hotmail.com>
2021-07-02 23:53:14 -0300

    erro-amb

--- src/main/java/com/crud/pessoas/crud/CrudPessoasApplication.java
+++ src/main/java/com/crud/pessoas/crud/CrudPessoasApplication.java
@@ -1,11 +1,20 @@
 package com.crud.pessoas.crud;
 
+import javax.swing.Spring;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class CrudPessoasApplication {
+@SpringBootApplication(scanBasePackages = "com.crud.pessoas.crud")
+public class CrudPessoasApplication extends SpringBootServletInitializer{
 
+	
+
+		
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudPessoasApplication.class, args);
 	}
